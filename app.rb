3a827bb0b169d226185c74960ebda8b24cd7f88f
Ubuntu <ubuntu@ip-10-170-101-239.us-west-1.compute.internal>
2013-02-09 04:54:34 +0000

    Now receiving data from the imp!!

--- app.rb
+++ app.rb
@@ -3,16 +3,30 @@ require 'bundler/setup' #Clears all gems from the load path EXCEPT those in the 
 require 'sinatra'
 require 'rack-flash'
 
+# helper methods
+require_relative './helpers.rb'
+
+configure do
+  #https://api.electricimp.com/v1/15c991e47cd2d556/305b47ad02f145bf
+  set :led_state, 
+end
 
 get '/' do
   #erb 'Hello!'
   erb :home
 end
 
-post '/led_state' do
+get '/led-state' do
+  'GET to led-state'
+end
+
+post '/led-state' do
   logger.info "Setting led_state..."
-  session[:led_state] = params[:value]
-  logger.info "Set led_state to: " + params[:value].to_s
+  logger.info "POST to /led-state HAS BEEN RECOREDED"
+  logger.info "value: " + params[:value].to_s
+  logger.info "target: " + params[:target].to_s
+  logger.info "channel: " + params[:channel].to_s
+  logger.info "DONE WITH POST"
 end
 
 # view helpers
