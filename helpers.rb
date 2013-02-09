90f6a40337b9deb66167694e75e53d95f2c8c7f7
Ben <theunixbeard@gmail.com>
2013-02-08 21:14:18 -0800

    added helpers.rb

---
+++ helpers.rb
@@ -0,0 +1,21 @@
+# view helpers
+
+def is_led_on
+  html = ''
+  if session[:led_state] == 1
+    html << 'active'
+  end
+  html
+end
+
+# other helpers
+
+def current_led_status
+  uri = URI.parse 'https://api.electricimp.com/v1/15c991e47cd2d556/305b47ad02f145bf?value=1&channel=2'
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = (uri.scheme == 'https')
+  request = Net::HTTP::Post.new(uri.request_uri)
+  request.set_form_data({"value" => "-1", "channel" => "2"})
+  response = http.request(request)
+  puts response
+end
